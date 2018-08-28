95813e4b56bec91e20780e4f524aed178b575d82
LwinMeMeKhaing.github.io <www.lwinmeme@gmail.com>
2018-08-28 14:25:32 +0530

    added files

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+	
+@app.route('/political')
+def political():
+		return render_template('political.html')
+
+@app.route('/man')
+def man():
+    return render_template('man.html')
+
+@app.route('/writer')
+def writer():
+    return render_template('writer.html')
+
+@app.route('/actor')
+def actor():
+    return render_template('actor.html')
+
+@app.route('/designer')
+def designer():
+    return render_template('designer.html')
+
+if __name__ == "__main__" :
+   app.route(Debug=True)
